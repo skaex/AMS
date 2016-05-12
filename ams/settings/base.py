7c51a69fa3cc08d68019c6023d1516118958b431
TeyimPila <teyim.pila@aun.edu.ng>
2016-05-12 13:42:48 +0100

    ignored secret variables

--- ams/settings/base.py
+++ ams/settings/base.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 from django.core.exceptions import ImproperlyConfigured
+from ..secrete_variables import *
 
 from secret.secret import *
 
