e6b58f0d472bc00a395d57d2aa119d124afb5d5e
TeyimPila <teyim.pila@aun.edu.ng>
2016-05-18 17:57:17 +0100

    Wrote the import form

---
+++ attendance/forms.py
@@ -0,0 +1,18 @@
+import os
+
+from django import forms
+
+IMPORT_FILE_TYPES = ['.xls', '.xlsx']
+
+
+class AttendanceImportForm(forms.Form):
+    input_excel = forms.FileField(required=True, label="Upload the attendance Excel file.")
+
+    def clean_input_excel(self):
+        input_excel = self.cleaned_data['input_excel']
+        extension = os.path.splitext(input_excel.name)[1]
+        if not (extension in IMPORT_FILE_TYPES):
+            raise forms.ValidationError(
+                '{} is not a valid excel file. Please make sure your input file is an excel file'.format(extension))
+        else:
+            return input_excel
